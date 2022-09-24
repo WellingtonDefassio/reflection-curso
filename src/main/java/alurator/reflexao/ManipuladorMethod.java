c811b684988e9bd5479c9daccc48c8ca941bec53
Wellington Defassio <well.defassio@gmail.com>
2022-09-24 15:06:29 -0300

    reflector - entendendo como frameworks funcionam .

--- src/main/java/alurator/reflexao/ManipuladorMethod.java
+++ src/main/java/alurator/reflexao/ManipuladorMethod.java
@@ -2,22 +2,32 @@ package alurator.reflexao;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class ManipuladorMethod {
 
 
     private Object instancia;
     private Method method;
+    private Map<String, Object> queryParams;
 
-    public ManipuladorMethod(Object instancia, Method method) {
+    public ManipuladorMethod(Object instancia, Method method, Map<String, Object> queryParams) {
         this.instancia = instancia;
 
         this.method = method;
+        this.queryParams = queryParams;
     }
 
     public Object invoca() {
         try {
-          return method.invoke(instancia);
+            List<Object> parametros = new ArrayList<>();
+            Stream.of(method.getParameters())
+                    .forEach(p-> parametros.add(queryParams.get(p.getName())));
+
+          return method.invoke(instancia, parametros.toArray());
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
--- src/main/java/alurator/reflexao/ManipuladorObjeto.java
+++ src/main/java/alurator/reflexao/ManipuladorObjeto.java
@@ -3,6 +3,7 @@ package alurator.reflexao;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class ManipuladorObjeto {
     private Object instancia;
@@ -15,15 +16,15 @@ public class ManipuladorObjeto {
     public ManipuladorMethod getMethod(String nomeMethod, Map<String, Object> queryParams) {
 
         Method[] declaredMethods = instancia.getClass().getDeclaredMethods();
-        Method metodoSelecionado = Arrays.stream(declaredMethods).filter(metodo -> true)
+        Method metodoSelecionado = Arrays.stream(declaredMethods).filter(metodo ->
+                        metodo.getName().equals(nomeMethod)
+                                && metodo.getParameterCount() == queryParams.values().size()
+                                && Stream.of(metodo.getParameters()).allMatch(param -> queryParams.keySet().contains(param.getName())
+                                && queryParams.get(param.getName()).getClass().equals(param.getType())))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("metodo não encontrado!"));
 
-        try {
-            Method method = instancia.getClass().getDeclaredMethod(nomeMethod);
-            return new ManipuladorMethod(instancia, method);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
+
+        return new ManipuladorMethod(instancia, metodoSelecionado, queryParams);
     }
 }
